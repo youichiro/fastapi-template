b40095742c54bf9903086b7a02dc9a457e216a43
youichiro <cinnamon416@gmail.com>
2023-01-27 23:54:33 +0900

    add database.py

---
+++ database.py
@@ -0,0 +1,11 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+SQLALCHEMY_DATABASE_URL = "mysql://cale:p@assword@0.0.0.0:3306/cale"
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
+
