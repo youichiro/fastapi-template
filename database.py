6bc9222d5d1b663db9e3a297a7594c5298f0a224
youichiro <cinnamon416@gmail.com>
2023-01-28 00:02:31 +0900

    change db name

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "mysql://cale:p@assword@0.0.0.0:3306/cale"
+SQLALCHEMY_DATABASE_URL = "mysql://cale:p@assword@0.0.0.0:3306/cale_development"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
