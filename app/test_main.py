ea638b732d7f9e57c14e1bb367611dd575bdb0e3
youichiro <cinnamon416@gmail.com>
2023-01-28 19:22:07 +0900

    create test_main.py

---
+++ app/test_main.py
@@ -0,0 +1,25 @@
+from fastapi.testclient import TestClient
+
+from .main import app
+
+client = TestClient(app)
+
+
+def test_create_accounts():
+    response = client.post(
+        "/v1/accounts",
+        json={
+            "admin_secret": "demo_secret",
+             "accounts": [
+                {
+                    "external_user_id": "example_external_user_1",
+                    "school_id": 1,
+                },
+                {
+                    "external_user_id": "example_external_user_2",
+                    "school_id": 2,
+                },
+            ]
+        }
+    )
+    assert response.status_code == 201
