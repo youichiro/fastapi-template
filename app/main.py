4408a31e11ce2dbb1d4231d2699036bb8db94ac7
youichiro <cinnamon416@gmail.com>
2023-01-28 16:45:48 +0900

    create dependencies.py

--- app/main.py
+++ app/main.py
@@ -1,28 +1,19 @@
 from fastapi import Depends, FastAPI, HTTPException
 from sqlalchemy.orm import Session
 
-from . import crud, models, schemas
-from .database import SessionLocal, engine
+from . import crud, models, schemas, dependencies
+from .database import engine
 
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
 
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
-
-
 @app.get("/v1/admin_accounts", response_model=list[schemas.AdminAccount])
-def get_admin_accounts(db: Session = Depends(get_db)):
+def get_admin_accounts(db: Session = Depends(dependencies.get_db)):
     return crud.get_admin_accounts(db)
 
 
 @app.post("/v1/accounts", response_model=schemas.Account)
-def create_account(account: schemas.AccountCreate, db: Session = Depends(get_db)):
+def create_account(account: schemas.AccountCreate, db: Session = Depends(dependencies.get_db)):
     return crud.create_account(db=db, account=account)
-
