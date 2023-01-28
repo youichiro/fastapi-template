693c567fae090a6f67228c2a70fc8e0ed0e59531
youichiro <cinnamon416@gmail.com>
2023-01-28 11:31:04 +0900

    mv to app

--- app/models.py
+++ app/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Boolean, Column, ForeignKey, Integer, String, Datetime, UniqueConstraint
+from sqlalchemy import Column, ForeignKey, Integer, String, DateTime, UniqueConstraint
 from sqlalchemy.orm import relationship
 from sqlalchemy.sql.expression import text
 
@@ -22,15 +22,15 @@ class Account(Base):
 
     id = Column(Integer, primary_key=True, autoincrement=True)
     admin_account_id = Column(Integer, ForeignKey("admin_accounts.id"), nullable=False)
-    external_user_id = db.Column(db.String(225), nullable=False, unique=True, index=True)
-    school_id = db.Column(db.Integer, nullable=False, index=True)
+    external_user_id = Column(String(225), nullable=False, unique=True, index=True)
+    school_id = Column(Integer, nullable=False, index=True)
     created_at = Column(DateTime, nullable=False, server_default=text("CURRENT_TIMESTAMP"))
     updated_at = Column(DateTime, nullable=False, server_default=text("CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP"))
 
     admin_account = relationship("AdminAccount", back_populates="accounts")
 
     __table_args__ = (
-        db.UniqueConstraint("admin_account_id", "external_user_id"),
+        UniqueConstraint("admin_account_id", "external_user_id"),
     )
 
 
