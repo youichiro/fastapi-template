1aa022046721e88d3e08ac7380c6f2c235e62208
youichiro <cinnamon416@gmail.com>
2023-01-28 16:41:50 +0900

    fix create_account api

--- app/crud.py
+++ app/crud.py
@@ -1,22 +1,43 @@
+from fastapi import HTTPException
 from sqlalchemy.orm import Session
 
 from . import models, schemas
 
+
+def get_admin_account(db: Session, admin_account_id: int):
+    return db.query(models.AdminAccount).filter(models.AdminAccount.id == admin_account_id).first()
 
 def get_admin_accounts(db: Session):
     return db.query(models.AdminAccount).all()
 
 
 def get_account(db: Session, admin_account_id: int, external_user_id: str):
-    account = db.query(models.Account).filter(
+    return db.query(models.Account).filter(
         models.Account.admin_account_id == admin_account_id,
         models.Account.external_user_id == external_user_id
     ).first()
-    return account
 
 
 def create_account(db: Session, account: schemas.AccountCreate):
-    new_account = models.Account()
+    db_admin_account = get_admin_account(db, account.admin_account_id)
+    if not db_admin_account:
+        raise HTTPException(
+            status_code=404,
+            detail=f"Not found admin_account id: {account.admin_account_id}."
+        )
+
+    db_account = get_account(db, account.admin_account_id, account.external_user_id)
+    if db_account:
+        raise HTTPException(
+            status_code=400,
+            detail=f"Account admin_account_id: {account.admin_account_id}, external_user_id: {account.external_user_id} already exists."
+        )
+
+    new_account = models.Account(
+        admin_account_id=account.admin_account_id,
+        external_user_id=account.external_user_id,
+        school_id=account.school_id,
+    )
     db.add(new_account)
     db.commit()
     db.refresh(new_account)
--- app/main.py
+++ app/main.py
@@ -19,17 +19,10 @@ def get_db():
 
 @app.get("/v1/admin_accounts", response_model=list[schemas.AdminAccount])
 def get_admin_accounts(db: Session = Depends(get_db)):
-    admin_accounts = crud.get_admin_accounts(db)
-    return admin_accounts
+    return crud.get_admin_accounts(db)
 
 
 @app.post("/v1/accounts", response_model=schemas.Account)
 def create_account(account: schemas.AccountCreate, db: Session = Depends(get_db)):
-    account = crud.get_account(db, account.admin_account_id, account.external_user_id)
-    if account:
-        HTTPException(
-            status_code=400,
-            detail=f"Account admin_account_id: {account.admin_account_id}, external_user_id: {account.external_user_id} already exists."
-        )
     return crud.create_account(db=db, account=account)
 
