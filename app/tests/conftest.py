a2fe882da6b0ea79503dd4d95fa301bd13a20457
youichiro <cinnamon416@gmail.com>
2023-01-29 13:06:57 +0900

    change test db env

--- app/tests/conftest.py
+++ app/tests/conftest.py
@@ -10,7 +10,14 @@ from app.main import app, get_db
 
 @pytest.fixture
 def test_db():
-    engine = create_engine(os.environ["SQLALCHEMY_DATABASE_URL"])
+    db_user = "root"
+    db_password = os.environ["MYSQL_ROOT_PASSWORD"]
+    db_host = os.environ["MYSQL_HOST"]
+    db_port = os.environ["MYSQL_PORT"]
+    db_name = os.environ["MYSQL_TEST_DATABASE"]
+    sqlalchemy_database_url = f"mysql://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}"
+
+    engine = create_engine(sqlalchemy_database_url)
     TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
     Base.metadata.create_all(bind=engine)
 
