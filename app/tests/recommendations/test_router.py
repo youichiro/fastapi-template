79be138e17b848cff92771974e3e5c8b9845ec8a
youichiro <cinnamon416@gmail.com>
2023-03-02 09:31:30 +0900

    add testcase

--- app/tests/recommendations/test_router.py
+++ app/tests/recommendations/test_router.py
@@ -9,24 +9,26 @@ from app.recommendations.schemas import answer_schema
 client = TestClient(app)
 
 
+json_dict = {
+    "answers": [
+        {
+            "section_code": "section_code_111",
+            "is_correct": False,
+        },
+        {
+            "section_code": "section_code_222",
+            "is_correct": False,
+        },
+        {
+            "section_code": "section_code_333",
+            "is_correct": True,
+        },
+    ]
+}
+
+
 def test_create_answers(db, mocker):
     mock = mocker.patch("app.recommendations.usecases.create_answers_usecase.exec", return_value=None)
-    json_dict = {
-        "answers": [
-            {
-                "section_code": "section_code_111",
-                "is_correct": False,
-            },
-            {
-                "section_code": "section_code_222",
-                "is_correct": False,
-            },
-            {
-                "section_code": "section_code_333",
-                "is_correct": True,
-            },
-        ]
-    }
     response = client.post("/v1/accounts/1/answers", json=json_dict)
 
     body = answer_schema.AnswerCreateInput.parse_obj(json_dict)
@@ -36,24 +38,17 @@ def test_create_answers(db, mocker):
 
 def test_create_answers_with_404(db, mocker):
     mock = mocker.patch("app.recommendations.usecases.create_answers_usecase.exec", side_effect=HTTPException(status_code=404, detail=f"error_detail"))
-    json_dict = {
-        "answers": [
-            {
-                "section_code": "section_code_111",
-                "is_correct": False,
-            },
-            {
-                "section_code": "section_code_222",
-                "is_correct": False,
-            },
-            {
-                "section_code": "section_code_333",
-                "is_correct": True,
-            },
-        ]
-    }
     response = client.post("/v1/accounts/1/answers", json=json_dict)
 
     body = answer_schema.AnswerCreateInput.parse_obj(json_dict)
     mock.assert_called_once_with(db, 1, body)
     assert response.status_code == 404
+
+
+def test_create_answers_with_SQLAlchemyError(db, mocker):
+    mock = mocker.patch("app.recommendations.usecases.create_answers_usecase.exec", side_effect=SQLAlchemyError())
+
+    with pytest.raises(SQLAlchemyError) as e:
+        client.post("/v1/accounts/1/answers", json=json_dict)
+
+    assert isinstance(e.value, SQLAlchemyError)
